5123177e17d6caae94c78612180403b7c3bdb000
b2jtheone <b2jtheone@gmail.com>
2022-08-24 18:57:52 +0200

    Finish CRUD actions

--- DapperASPNetCore/Program.cs
+++ DapperASPNetCore/Program.cs
@@ -1,8 +1,14 @@
+using DapperASPNetCore.Context;
+using DapperASPNetCore.Contracts;
+using DapperASPNetCore.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
